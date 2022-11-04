9a1e3b4ea8c77063e953a7c151576750253a068c
Gabriel Catonho Gomes <gabriel_endo23@hotmail.com>
2022-11-04 00:04:48 -0300

    login-esqueleto

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+              private usuarioService:UsuarioService,
+              ) { }
+  usuario: Usuario ={};
+  erro!:string;
+  storage: Storage = localStorage
 
   ngOnInit(): void {
+    this.storage.removeItem(`usuario`);
   }
-
+  login(usuario:Usuario):void{
+    let user = usuario.cpf
+    let senha= usuario.senha
+    if(user != null && senha != null){
+      this.usuarioService.getUsuarioApi(user).subscribe((res) => {
+        if(res.cpf == user && res.senha == senha){
+          this.storage.setItem(`usuario`, user as string)
+          this.router.navigate([`/dashboard`])
+        }
+        if(user == `admin` && senha ==`admin`){
+          localStorage.setItem(`admin`, `admin`)
+          //colocar o link de redirect do adm
+        }
+        else{
+          this.erro = "Usuario ou senha invalidos"
+        }
+      });
+    }
+  }
+  validaCpf(cpf:string):boolean{
+    const CPF = require('cpf');
+    return CPF.isValid(cpf)
+  }
+  isCpf():boolean{
+    return this.validaCpf(this.usuario.cpf as string)
+  }
 }
